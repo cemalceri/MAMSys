bb16588dd13d147e3f1b821fc98384851c1d3433
cemalceri <cemalceri@kik.gov.tr>
2020-01-07 17:59:54 +0300

    loggin 13. video sonu

---
+++ MAMSys.Core/CrossCuttingConcern/Logging/Log4Net/LoggerServiceBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using log4net;
+using log4net.Repository;
+
+namespace MAMSys.Core.CrossCuttingConcern.Logging.Log4Net
+{
+    public class LoggerServiceBase
+    {
+        private ILog _log;
+        public LoggerServiceBase(string name)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(File.OpenRead("log4net.config"));
+
+            ILoggerRepository loggerRepository = LogManager.CreateRepository(Assembly.GetEntryAssembly(),
+                typeof(log4net.Repository.Hierarchy.Hierarchy));
+            log4net.Config.XmlConfigurator.Configure(loggerRepository, xmlDocument["log4net"]);
+            _log = LogManager.GetLogger(loggerRepository.Name, name);
+        }
+    }
+}
